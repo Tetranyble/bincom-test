4d9f3c9cdff45544e1beae3d8b7432d5190f049c
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2021-01-08 20:47:32 +0000

    initial commit with controller folder and electionResultController created

---
+++ controller/resultController.js
@@ -0,0 +1,12 @@
+'use strict'
+const models = require('../models');
+
+const ElectrionResultController = () => {
+    return {
+        pollingUnitResult: ( req, res ) => {},
+        allPollingUnitsInLocalGovermentResults: ( req, res ) =>{},
+        storePollingUnitResult: ( req, res ) =>{}
+    }
+}
+
+module.exports = ElectrionResultController();
